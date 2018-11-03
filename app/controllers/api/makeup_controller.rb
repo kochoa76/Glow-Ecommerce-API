91013d1d0ce0278cd39ece1388b051737ad9add7
github username <github email address>
2018-11-03 16:05:56 -0500

    adding to makeup controller methods

---
+++ app/controllers/api/makeup_controller.rb
@@ -0,0 +1,47 @@
+class Api::MakeupController < ApplicationController
+before_action :set_makeup, only: [:show, :update, :destroy]
+
+  def index
+    render json: Makeup.all
+  end
+
+  def create
+    makeup = Makeup.new(makeup_params)
+    if makeup.save
+      render json: makeup
+    else
+      render json: {message: makeup.errors}, status: 400
+    end
+  end
+
+  def show
+    render json: @makeup
+  end
+
+  def update
+    if @makeup.update(makeup_params)
+      render json: @makeup
+    else
+      render json: {message: @makeup.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @makeup.destroy
+      render status: 204
+    else
+      render json: {message: "Unable to remove this makeup"}, status: 400
+    end
+  end
+
+
+  private
+
+  def makeup_params
+    params.require(:makeup).permit(:name, :price, :img_url, :description)
+  end
+
+  def set_makeup
+    @makeup = Makeup.find_by(id: params[:id] )
+  end
+end
